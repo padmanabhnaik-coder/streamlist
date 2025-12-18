e0236f7c666fcc5216be446c6ea736f90be5ed3f
padmanabhnaik-coder <padmanabh.naik@gmail.com>
2025-12-18 15:55:48 +0530

    renamed mew_task to new_task

--- task_mgr.py
+++ task_mgr.py
@@ -7,7 +7,7 @@ if "tasks" not in st.session_state:
     st.session_state.tasks=[]
 
 #add new task
-mew_task=st.text_input("Enter a new task:")
+new_task=st.text_input("Enter a new task:")
 if st.button("add_task") and new_task:
     st.session_state.tasks.append({"name":new_task,"done":False})
 
@@ -18,4 +18,4 @@ for i, task in enumerate(st.session_state.tasks):
     is_done=st.checkbox(task["name"],value=task["done"], key=i)
     st.session_state.tasks[i]["done"]="is_done"
 
-    
+
